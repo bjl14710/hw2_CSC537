b843cd06d8a9b366cb08a1b5f76f7e3a0eee8d0b
bjl14710 <bjl14710@gmail.com>
2021-09-16 19:08:28 -0600

    added code from teacher on d2l. "NEED TO IMPLEMENT"

--- problem1.py
+++ problem1.py
@@ -1,4 +1,8 @@
-import TkinterPoints
+# import TkinterPoints
+import seg_inter
+import RedBlackTree
+import DCEL
+
 
 class point:
 
@@ -15,6 +19,10 @@ class segment:
     def slope(self):
         return (self.point1.y-self.point2.y)/(self.point1.x-self.point2.y)
 
+    def createSegment(self):
+        return (self.point1,self.point2)
+
+    
 
 #for Queing the segments
 class Queue:
@@ -35,23 +43,57 @@ class Queue:
         return len(self.items)
     
     def printItems(self):
-        print(self.items)
+        print(str(self.items))
+
+
+def compare(p1, p2):
+    if p1.x < p2.x:
+        return -1
+    elif p1.x > p2.x:
+        return 1
+  
+    if p1.x == p2.x:
+        if p1.ptype == p2.ptype:
+            if p1.y < p2.y:
+                return -1
+            else:
+                return 1
+        else:
+            if p1.ptype == 0:
+                return -1
+            else:
+                return 1
+
+
+
+
+
+
 
 
 test = segment((1,1),(2,2))
+
 print(test.slope())
 
+
+#2 segments with one intersection
+s1 = segment((1,1),(10,10))
+s2 = segment((2,1),(10,5))
 Q = Queue()
 
-print(Q.isEmpty())
+#print(Q.isEmpt.y())
 
 #yay it works.
-Q.enqueue(test)
-Q.enqueue(111)
+#printing doesn't though :/
+Q.enqueue(s1)
+Q.enqueue(s2)
 Q.enqueue(123)
 Q.dequeue()
 print(Q.size())
-Q.printItems()
+
+
+
+# Q.printItems()
 
 
 
