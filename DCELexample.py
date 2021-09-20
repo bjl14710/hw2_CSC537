300321da78ebb0787cb0a406a07ae21ad03165d4
Brandon <bjl14710@gmail.com>
2021-09-20 01:25:41 -0700

    mmmmm

--- DCELexample.py
+++ DCELexample.py
@@ -1,8 +1,10 @@
 from RedBlackTree import *
-from dcel1 import *
+# from dcel1 import *
 import math 
 import random
 from tkinter import *
+from DCEL import *
+
 
 YSIZE = 1000
 PSIZE = 4
@@ -25,6 +27,22 @@ def find_intersections(event):
     print(segs)
     drawSegments(segs)
     find_inters(segs)
+
+def line_intersection(line1, line2):
+    xdiff = (line1[0][0] - line1[1][0], line2[0][0] - line2[1][0])
+    ydiff = (line1[0][1] - line1[1][1], line2[0][1] - line2[1][1])
+
+    def det(a, b):
+        return a[0] * b[1] - a[1] * b[0]
+
+    div = det(xdiff, ydiff)
+    if div == 0:
+       raise Exception('lines do not intersect')
+
+    d = (det(*line1), det(*line2))
+    x = det(d, xdiff) / div
+    y = det(d, ydiff) / div
+    return x, y
 
 def find_inters(S):
     print("find_inters")  # code from Q1
